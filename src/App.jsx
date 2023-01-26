e8dba11db16cc07105b90ed924ac0772329b3b70
JaiderSalas <jsalasb1@unicartagena.edu.co>
2023-01-26 13:09:19 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ListaNoticias from './components/ListaNoticias'
+function App(){
+  return(
+    <div>
+    <ListaNoticias/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ListaNoticias.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+const ListaNoticias = () => {
+  const [Articulos,setArticulos] = useState([])
+
+  useEffect(() => {
+
+  })
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ListaNoticias
