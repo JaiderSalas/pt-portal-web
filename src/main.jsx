07a5f2a639c0268f5909e727aba2e87f581bb98c
JaiderSalas <jsalasb1@unicartagena.edu.co>
2023-01-27 01:19:58 -0500

    deploy4

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Login from "./pages/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Principal from "./pages/Principal";
-import Reportes from "./pages/Reportes";
+import App from "./pages/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/principal" element={<Principal />} />
-        <Route path="/reportes" element={<Reportes />} />
-      </Routes>
-    </BrowserRouter>
+     <App/>
   </React.StrictMode>
 );
---
+++ src/pages/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Login from './Login'
+import Principal from './Principal'
+import Reportes from './Reportes'
+function App() {
+  return (
+    <BrowserRouter>
+     <Routes>
+        <Route path="/pt-portal-web/" element={<Login/>} />
+        <Route path="/pt-portal-web/principal" element={<Principal/>} />
+        <Route path="/pt-portal-web/Reportes" element={<Reportes/>} />
+        </Routes>
+     </BrowserRouter>
+  )
+}
+
+export default App
