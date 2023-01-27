dc877a5f0cc11e9d35a6b7eb1fe67d903f819341
JaiderSalas <jsalasb1@unicartagena.edu.co>
2023-01-27 01:52:53 -0500

    deploy8

--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -1,22 +1,25 @@
+import React, {Component } from 'react'
 import EstadoTiempo from "../components/EstadoTiempo";
 import ListaNoticias from "../components/ListaNoticias";
 import '../styles/estilos.css'
 import Reportes from "./Reportes";
-function Principal() {
-  return (
-    <div>
-    <div>
-      <div>
-        <h1 className="title">Noticias</h1>
-        <ListaNoticias />
-      </div>
-      <div className="estado">
-      <EstadoTiempo />
-      </div>
-    </div>
-    <Reportes/>
-    </div>
-  );
-}
 
-export default Principal;
+export default class Principal extends Component {
+  render(){
+    return (
+      <div>
+      <div>
+        <div>
+          <h1 className="title">Noticias</h1>
+          <ListaNoticias />
+        </div>
+        <div className="estado">
+        <EstadoTiempo />
+        </div>
+      </div>
+      <Reportes/>
+      </div>
+    );
+  }
+ 
+}
