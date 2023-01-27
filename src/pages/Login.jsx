5c174c7ad637b170796c197b2ecb0b843609f925
JaiderSalas <jsalasb1@unicartagena.edu.co>
2023-01-27 06:13:12 -0500

    try2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,7 +25,7 @@ export default class Login extends Component {
     usuarios.usuarios.map((usuario)=>{
       if(usuario.usuario === this.state.form.usuario && usuario.contraseña === this.state.form.contraseña){
         alert("Sesion Iniciada")
-        window.location.href="/main"
+        window.location.href="/pt-portal-web/main"
       }
       else{
           alert("Usuario o contraseña Incorrectas")
