b2959ff385d19f2f6c385a2dad9c09a63960039d
JaiderSalas <jsalasb1@unicartagena.edu.co>
2023-01-27 00:29:18 -0500

    deploy3

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Login from './pages/Login'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Principal from "./pages/Principal";
+import Reportes from "./pages/Reportes";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Login/>
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/principal" element={<Principal />} />
+        <Route path="/reportes" element={<Reportes />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
