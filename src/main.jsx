91739fbf2ce8f5d3050a15998a63b780e1af78e5
JaiderSalas <jsalasb1@unicartagena.edu.co>
2023-01-27 00:25:29 -0500

    deploy2

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Rutas from './Routes/Rutas'
+import Login from './pages/Login'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Rutas/>
+    <Login/>
   </React.StrictMode>,
 )
